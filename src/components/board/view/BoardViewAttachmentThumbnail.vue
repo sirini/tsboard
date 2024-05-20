4ca3d80c9ea6fe929176125c5d3c69d962ae852d
sirini <sirini@gmail.com>
2024-05-20 12:40:50 +0000

    썸네일 설명글 배경색 변경

--- src/components/board/view/BoardViewAttachmentThumbnail.vue
+++ src/components/board/view/BoardViewAttachmentThumbnail.vue
@@ -4,7 +4,7 @@
       <v-col :cols="home.cols" v-for="(thumb, index) in view.thumbs" :key="index">
         <v-card elevation="0">
           <v-img cover :src="thumb"></v-img>
-          <v-card-text>{{ view.descriptions[index] }}</v-card-text>
+          <v-card-text class="description pa-3">{{ view.descriptions[index] }}</v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -19,3 +19,10 @@ import { useHomeStore } from "../../../store/home"
 const view = useBoardViewStore()
 const home = useHomeStore()
 </script>
+
+<style scoped>
+.description {
+  background-color: #f8fafb;
+  line-height: 1.6em;
+}
+</style>
