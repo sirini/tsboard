4364fbdf2a54c34cedf1b5fb6cc839a8c7d3a1cc
Heegeun Park <senstagram.com@gmail.com>
2024-08-17 15:07:40 +0900

    의존성 패키지 업데이트, 글보기시 첨부 이미지 썸네일 클릭하면 크게 보기 기능 추가

--- src/components/board/view/BoardViewAttachmentThumbnail.vue
+++ src/components/board/view/BoardViewAttachmentThumbnail.vue
@@ -2,7 +2,7 @@
   <div class="pa-6" v-if="view.images.length > 0">
     <v-row>
       <v-col :cols="home.cols" v-for="(image, index) in view.images" :key="index">
-        <v-card elevation="0">
+        <v-card elevation="0" @click="view.openPreviewDialog(TSBOARD.PREFIX + image.file.path)">
           <v-img cover :src="TSBOARD.PREFIX + image.thumbnail.small"></v-img>
           <v-card-text class="description pa-3" v-if="view.images[index].description.length > 0">{{
             view.images[index].description
